03e79d7b7fe3268942911b9e79db0839c82cd879
frank <frank@666.com>
2023-08-08 15:25:06 +0800

    commitAllFileFirst

---
+++ src/main/java/elon/musk/Main.java
@@ -0,0 +1,16 @@
+package elon.musk;
+
+/**
+ * @projectName: ${PROJECT_NAME}
+ * @package: elon.musk
+ * @className: ${NAME}
+ * @author: Chen
+ * @description: TODO
+ * @date: ${DATE} ${TIME}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello this master test!!!1");
+        System.out.println("Hello world!");
+    }
+}
